5b9fcf0da4ea58f9961a0e7663a68974715dfe2c
matkopapic <matko.papic@gmail.com>
2025-04-26 10:42:59 +0200

    Disable authentication on public routes

--- src/main/kotlin/com/meally/backend/config/SecurityConfig.kt
+++ src/main/kotlin/com/meally/backend/config/SecurityConfig.kt
@@ -9,19 +9,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
 
 @Configuration
 class SecurityConfig(
     private val userService: UserService
 ) {
 
+    private val publicRoutes: RequestMatcher = AntPathRequestMatcher("/public/**")
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
         http
             .csrf { it.disable() }
             .authorizeHttpRequests {
-                it.anyRequest().authenticated()
+                it
+                    .requestMatchers(publicRoutes).permitAll()
+                    .anyRequest().authenticated()
             }
-            .addFilterBefore(FirebaseAuthenticationFilter(userService), UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterBefore(FirebaseAuthenticationFilter(publicRoutes, userService), UsernamePasswordAuthenticationFilter::class.java)
             .build()
 }
